0618d7ea5920779305a89c50655d9dcd89aac891
Bill Feigal <Bill Feigal@HomeDesktop>
2017-08-22 19:43:42 -0500

    Setting up for unit 2 code

--- src/Week1/test.java
+++ src/Week1/test.java
@@ -3,7 +3,18 @@ package Week1;
 public class test {
 
 	public static void main( String args[])  {
-		System.out.println("Hello Sweet Pea!");
+		int n = 20;
+		int count = 0;
+		int i=0, j=0;
+		do {
+		    do {
+		          System.out.println("...looping..." + count);
+		          count += 1;//growth should be measured in calls to println. 
+		          j=j+5;
+		     } while (j < n);
+		     i++;
+		     j = 0;
+		} while (i < n);
 	}
 	
 	
---
+++ src/Week2_Unit2/ser222_unit2b_hw02_base.java
@@ -0,0 +1,46 @@
+package Week2_Unit2;
+
+public class ser222_unit2b_hw02_base {
+//TODO: implement interface.
+    
+    
+    /**
+     * Entry point for matrix testing.
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int[][] data1 = new int[0][0];
+        int[][] data2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] data3 = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
+        
+        Matrix m1 = new ser222_unit12b_hw02_base(data1);
+        Matrix m2 = new ser222_unit12b_hw02_base(data2);
+        Matrix m3 = new ser222_unit12b_hw02_base(data3);
+        
+        System.out.println("m1 --> Rows: " + m1.getRows() + " Columns: " + m1.getColumns());
+        System.out.println("m2 --> Rows: " + m2.getRows() + " Columns: " + m2.getColumns());
+        System.out.println("m3 --> Rows: " + m3.getRows() + " Columns: " + m3.getColumns());
+        
+        //check for reference issues
+        System.out.println("m2 -->\n" + m2);
+        data2[1][1] = 101;
+        System.out.println("m2 -->\n" + m2);
+
+        //test equals
+        System.out.println("m2==null: " + m2.equals(null));             //false
+        System.out.println("m3==\"MATRIX\": " + m2.equals("MATRIX"));   //false
+        System.out.println("m2==m1: " + m2.equals(m1));                 //false
+        System.out.println("m2==m2: " + m2.equals(m2));                 //true
+        System.out.println("m2==m3: " + m2.equals(m3));                 //false
+        
+        //test operations (valid)
+        System.out.println("2 * m2:\n" + m2.scale(2));
+        System.out.println("m2 + m3:\n" + m2.plus(m3));
+        System.out.println("m2 - m3:\n" + m2.minus(m3));
+        
+        //test operations (invalid)
+        //System.out.println("m1 + m2" + m1.plus(m2));
+        //System.out.println("m1 - m2" + m1.minus(m2));
+    }
+}
