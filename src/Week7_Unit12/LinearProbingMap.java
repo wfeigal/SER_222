d8af836c760ae1b843909bcfef5bd206063f4a06
Bill Feigal <Bill Feigal@HomeDesktop>
2017-09-28 19:51:51 -0500

    Set up for Unit 12

---
+++ src/Week7_Unit12/LinearProbingMap.java
@@ -0,0 +1,50 @@
+package Week7_Unit12;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinearProbingMap<Key,Value> implements Map{
+
+	@Override
+	public void put(Object key, Object val) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Object key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterable keys() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
